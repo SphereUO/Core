82ae11f7a7114e83918a83776442fe3bcf5cbbb8
Raylde <50953519+Raylde@users.noreply.github.com>
2023-12-15 10:24:54 +0100

    Remove unnecesary property name title, this better manage via script

--- src/game/items/CItem.cpp
+++ src/game/items/CItem.cpp
@@ -1756,56 +1756,6 @@ lpctstr CItem::GetNameFull( bool fIdentified ) const
 		len += snprintf(pTemp + len, Str_TempLength() - len, pszTitle, GetAmount());
 	}
 
-	if ( fIdentified && IsAttr(ATTR_CURSED|ATTR_CURSED2|ATTR_BLESSED|ATTR_BLESSED2|ATTR_MAGIC))
-	{
-		bool fTitleSet = false;
-		switch ( m_Attr & ( ATTR_CURSED|ATTR_CURSED2|ATTR_BLESSED|ATTR_BLESSED2))
-		{
-			case (ATTR_CURSED|ATTR_CURSED2):
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_UNHOLY );
-				fTitleSet = true;
-				break;
-			case ATTR_CURSED2:
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_DAMNED );
-				fTitleSet = true;
-				break;
-			case ATTR_CURSED:
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_CURSED );
-				fTitleSet = true;
-				break;
-			case (ATTR_BLESSED|ATTR_BLESSED2):
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_HOLY );
-				fTitleSet = true;
-				break;
-			case ATTR_BLESSED2:
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_SACRED );
-				fTitleSet = true;
-				break;
-			case ATTR_BLESSED:
-				pszTitle = g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_BLESSED );
-				fTitleSet = true;
-				break;
-		}
-		if ( fTitleSet )
-		{
-			if ( fSingular && !IsSetOF(OF_NoPrefix) )
-				len = Str_CopyLimitNull( pTemp, Str_GetArticleAndSpace(pszTitle), Str_TempLength());
-			len += Str_CopyLimitNull( pTemp+len, pszTitle, Str_TempLength() - len);
-		}
-
-		if ( IsAttr(ATTR_MAGIC))
-		{
-			if ( !pszTitle )
-			{
-				pszTitle = IsSetOF(OF_NoPrefix) ? "" : "a ";
-				len = Str_CopyLimitNull( pTemp, pszTitle, Str_TempLength());
-			}
-
-			if ( !IsTypeArmorWeapon() && (strnicmp( pszName, "MAGIC", 5 ) != 0))		// don't put "magic" prefix on armor/weapons and names already starting with "magic"
-				len += Str_CopyLimitNull( pTemp+len, g_Cfg.GetDefaultMsg( DEFMSG_ITEMTITLE_MAGIC ), Str_TempLength() - len);
-		}
-	}
-
 	// Prefix the name
 	switch ( m_type )
 	{
